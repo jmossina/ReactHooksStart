5c4333924b3b31172981da8e8fcbf66e9bc03586
Jacopo Mossina <Jacopo.Mossina@appway.com>
2019-04-09 10:35:26 +0200

    project setup, added Todo functional component

---
+++ src/components/Todo.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+const todo = props => {
+  const [todoName, setTodoName] = useState('');
+  const [todoList, setTodoList] = useState([]);
+
+  const inputChangeHandler = event => {
+    setTodoName(event.target.value);
+  };
+
+  const todoAddHandler = () => {
+    setTodoList(todoList.concat(todoName));
+  };
+
+  return (
+    <React.Fragment>
+      <input
+        type="text"
+        placeholder="Todo"
+        onChange={inputChangeHandler}
+        value={todoName}
+      />
+      <button onClick={todoAddHandler}>Add</button>
+      <ul>
+        {todoList.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+};
+
+export default todo;
