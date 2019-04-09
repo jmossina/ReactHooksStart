e07d48a944c7d61350003c1656fd466538740221
Jacopo Mossina <Jacopo.Mossina@appway.com>
2019-04-09 11:00:54 +0200

    Posting todo to Firebase on submit

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
+import axios from 'axios';
+
+const ENDPOINT_TODOS = 'https://react-hooks-start.firebaseio.com/todos.json';
 
 const todo = props => {
+  // !!! ONLY call useState on ROOT level of your function !!!
   const [todoName, setTodoName] = useState('');
   const [todoList, setTodoList] = useState([]);
+
+  // Alternative approach: unified state - SUBOPTIMAL
+  // Drawbacks: setState DOES NOT merge the state with the provided object, it completely
+  // replaces it: more code to be written, error prone
+  // const [state, setState] = useState({ userInput: '', todoList: [] });
 
   const inputChangeHandler = event => {
     setTodoName(event.target.value);
   };
 
-  const todoAddHandler = () => {
-    setTodoList(todoList.concat(todoName));
+  const todoAddHandler = async () => {
+    const updatedList = todoList.concat(todoName);
+    setTodoList(updatedList);
+    let response;
+    try {
+      response = await axios.post(ENDPOINT_TODOS, { name: todoName });
+    } catch (exception) {
+      console.error(exception.message);
+    } finally {
+      console.log(response);
+    }
   };
 
   return (
